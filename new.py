43d090c575f8ab13cd433ba0f6c672a933d7a904
VisaJE <eemil@live.com>
2018-01-14 21:11:11 +0200

    muutettu kestämään virheitä

--- new.py
+++ new.py
@@ -1,6 +1,29 @@
-print("Hello!")
-x = 10
-while x:
-  print("!")
-  x -= 1
-input()
+import time
+
+import telepot
+
+
+def handler(msg):
+	try:
+		chat_id = msg['chat']['id']
+		if msg['text'].lower().find('edistyykö') + 1:
+    
+			bot.sendMessage(
+				chat_id,
+				'No ei!',
+				reply_to_message_id=msg['message_id']
+			)
+		elif msg['text'].lower().find('toimiiko') + 1:
+			bot.sendMessage(
+					chat_id,
+					'Onhan tuo mahdollista.',
+					reply_to_message_id=msg['message_id']
+			)
+	except Exception:
+		time.sleep(10)
+		#Takes the token as a parameter.
+bot = telepot.Bot('540750088:AAGLEDAgYHG_aIqWSoneVGy6d7SlyJ1c3bY')
+bot.message_loop(handler)
+
+while 1:
+	time.sleep(10)
