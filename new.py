96b9224cd33301bf361ab1b4161004dd7cff5dc9
VisaJE <eemil@live.com>
2018-01-19 22:04:29 +0200

    added plot functionality

--- new.py
+++ new.py
@@ -1,26 +1,54 @@
 import time
+import random
 
+import matplotlib.pyplot as plt
 import telepot
 
-
+def makeplot():
+	x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	y = []
+	y2 = []
+	for i in x:
+		y.append(random.randint(0,10))
+		y2.append(random.randint(0,10))
+	plt.plot(x, y, x, y2)
+	try:
+		plt.savefig('kuva.png', bbox_inches='tight')
+	except Exception:
+		print("Creating the picture failed")
+		
+		
 def handler(msg):
 	try:
 		chat_id = msg['chat']['id']
 		if msg['text'].lower().find('edistyyk') + 1:
-    
+			print('Chat command issued')
 			bot.sendMessage(
 				chat_id,
 				'No ei!',
 				reply_to_message_id=msg['message_id']
 			)
 		elif msg['text'].lower().find('toimiiko') + 1:
+			print('Chat command issued')
 			bot.sendMessage(
 					chat_id,
 					'Onhan tuo mahdollista.',
 					reply_to_message_id=msg['message_id']
 			)
+		elif msg['text'].lower().find('plot') + 1:
+			print('Plot command issued')
+			makeplot()
+			with open('kuva.png', 'rb') as img:
+				bot.sendPhoto(
+					chat_id,
+					img,					
+					reply_to_message_id=msg['message_id']
+				)
+					
 	except Exception:
-		time.sleep(10)
+		pass
+		
+
 #Finds the token.
 with open('/opt/secrets/token.txt', 'r') as t:
     data = t.read().split()
