78aabf6993e5904a596543c0fdb460f719440f6b
VisaJE <eemil.visakorpi@gmail.com>
2018-01-23 17:56:26 +0200

    Not working

--- testi.py
+++ testi.py
@@ -9,40 +9,38 @@ hostname = 'localhost'
 db = 'testdb'
 table = 'testtable'
 
-_highscore = 0
-scoring_time = 0
-scorer = 'Nobody'
-
 connection = pgdb.connect(host = hostname,
                           user = username, 
                           database = db
                           )
-cur = connection.cursor()
 
 def get_highscore():
     try:
-        cur.execute("SELECT * FROM %s ORDER BY %s DESC"%(table, 'amount'))
-        return cur.fetchone()
+        cur = connection.cursor()
+        cur.execute("SELECT * FROM %s ORDER BY %s DESC, %s"%(table, 'amount', 'date'))
+        result = cur.fetchone()
+        cur.close()
+        return result
     except:
         print('There was a problem with fetching data from db.')
 
 def get_user(input_name):
     try:
-        cur.execute("SELECT max(amount) FROM %s WHERE name = %s"%(table,
-            input_name))
-        return cur.fetchone()
+        cur = connection.cursor()
+        cur.execute("SELECT * FROM %s WHERE name = %s ORDER BY %s DESC"%(table,
+            input_name, 'amount'))
+        result = cur.fetchone()
+        cur.close()
+        return result
     except:
         raise ValueError('No datapoints were found for this name.')
 
 def print_hs_data():
     result = get_highscore()
     score = result.amount
-    if result >  _highscore:
-        _highscore = score        
-        scorer = result.name
-        scoring_time = result.date
-    text =  'Kovin heiluttelija on %s indeksin arvolla %s.\n'%(scorer, highscore)
-    'Suoritus tehtiin ajanhetkella.'%(scoring_time)
+    time = result.date
+    text =  'Kovin heiluttelija on %s indeksin arvolla %s.\n'%(result.name,score)
+    'Suoritus tehtiin ajanhetkella.'%(time)
     return text
 
 def print_user_data(input_name):
@@ -55,12 +53,9 @@ def print_user_data(input_name):
 
 while 1:
     print('Starting test')
-    cur.execute("SELECT * FROM testtable")
-    print(cur.fetchone().name)
-    print(cur.fetchone()[0])
     nimi = input('Nimi?')
-    if (nimi != "Pekka"):
+    if (nimi == ''):
         print(print_hs_data())
     else:
-        print(print_user_data('Pekka'))
+        print(print_user_data(nimi))
         time.sleep(5)
