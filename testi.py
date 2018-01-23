1b3b22f8fd324530b3c94edbf0c38164bbdc5b9f
VisaJE <eemil.visakorpi@gmail.com>
2018-01-23 17:24:19 +0200

    More tests

--- testi.py
+++ testi.py
@@ -9,52 +9,58 @@ hostname = 'localhost'
 db = 'testdb'
 table = 'testtable'
 
-highscore = 0
+_highscore = 0
 scoring_time = 0
 scorer = 'Nobody'
 
 connection = pgdb.connect(host = hostname,
-                            user = username, 
-                            password = password, 
-                            database = db
-                            )
+                          user = username, 
+                          database = db
+                          )
 cur = connection.cursor()
 
 def get_highscore():
     try:
-        cur.execute("SELECT MAX(%s) FROM %s;"%('amount', table))
-        cur.fetchone()
+        cur.execute("SELECT * FROM %s ORDER BY %s DESC"%(table, 'amount'))
+        return cur.fetchone()
     except:
         print('There was a problem with fetching data from db.')
 
 def get_user(input_name):
     try:
-        cur.execute("SELECT * FROM %s HAVING name = %s AND MAX(%s);"%(table, input_name, 'amount'))
-        cur.fetchone
+        cur.execute("SELECT max(amount) FROM %s WHERE name = %s"%(table,
+            input_name))
+        return cur.fetchone()
     except:
         raise ValueError('No datapoints were found for this name.')
 
 def print_hs_data():
     result = get_highscore()
     score = result.amount
-    if result > highscore:
-        highscore = score        
+    if result >  _highscore:
+        _highscore = score        
         scorer = result.name
         scoring_time = result.date
-    'Kovin käsienheiluttelija on %s heilutteluindeksillä %s.\n'%(scorer, highscore)
-    'Ennätys tehtiin %s tuntia sitten.'%(scoring_time)
+    text =  'Kovin heiluttelija on %s indeksin arvolla %s.\n'%(scorer, highscore)
+    'Suoritus tehtiin ajanhetkella.'%(scoring_time)
+    return text
 
 def print_user_data(input_name):
     try:
         result = get_user(input_name)
-        'Tämä heppu heilutteli arvon %s %s.'%(result.amount, result.date)
+        return 'Kyseinen heppu heilutteli arvon %s.'%(result.amount)
     except Exception as e:
         e.args[0]
 
 
 while 1:
-    nimi = input('Name?')
+    print('Starting test')
+    cur.execute("SELECT * FROM testtable")
+    print(cur.fetchone().name)
+    print(cur.fetchone()[0])
+    nimi = input('Nimi?')
     if (nimi != "Pekka"):
         print(print_hs_data())
     else:
-    time.sleep(5)
+        print(print_user_data('Pekka'))
+        time.sleep(5)
