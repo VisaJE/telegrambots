b086aaaf8addd4bddeb68f1816a75e8a058059cc
VisaJE <eemil@live.com>
2018-01-24 17:47:08 +0200

    Soon to work

--- Inserter.py
+++ Inserter.py
@@ -2,14 +2,13 @@ import time
 import pgdb
 
 connection = pgdb.connect(host = 'localhost',
-                          user = 'testuser', 
+                          user = 'testuser',
                           database = 'testdb'
                           )
-i = input('How many? ')
+i = raw_input('How many? ')
 
 cur = connection.cursor()
 for n in range(1, i):
-    date = "timestamp('2018-%s-1' %s:00:00)"%(randomInt(23), randomInt(23))
-    cur.execute("INSERT INTO testtable VALUES(%s, %s, %s)"%(randomint(12),
-        randomint(100), date)
-connection.close()
+    date = "timestamp('2018-{}-1' {}:00:00)".format(randomint(23), randomint(23))
+    cur.execute("INSERT INTO testtable VALUES({}, {}, {})".format(randomint(12), \
+    randomint(100), date))
